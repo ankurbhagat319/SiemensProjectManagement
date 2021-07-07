92f90db52d04557ce3a5e5bea7fe578dc561b578
Uttam Bhagat, Ankur (ADV D AA DDI HMI-P S7PLC) <ankur.bhagat@siemens.com>
2021-07-07 18:55:21 +0530

    Edmx files

---
+++ SiemensProjectManagement/Controllers/AssetsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SiemensProjectManagement.Models;
+
+namespace SiemensProjectManagement.Controllers
+{
+    public class AssetsController : Controller
+    {
+        // GET: Assets
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Assets()
+        {
+            ProjectManagementDB db = new ProjectManagementDB();
+            var assets = db.AssetDetails.Select(x => x).Where(x => x.UserName == "z003tyvc").ToList();
+
+            //var tmp = from et in db.AssetDetails
+                 
+            //          select new
+            //          {
+            //            ProjectName =  et.Project.ProjectName,
+            //            HostName  = et.HostName,
+
+
+            //          };
+
+            TempData["assets"] = assets;
+            return View();
+        }
+    }
+}
---
+++ SiemensProjectManagement/Models/PlcVirtualizationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiemensProjectManagement.Models
+{
+    public class PlcVirtualizationModel
+    {
+
+               public int? PlcId { get; set; }
+               public string Rack_No             {get;set;}
+              public string Plc_SerialNo { get; set; }
+               public string IpAddress           {get;set;}
+               public int? DeviceID            {get;set;}
+               public string PLC_Name            {get;set;}
+               public string PLC_Type            {get;set;}
+               public string Mlfb                {get;set;}
+               public string Mac_Address         {get;set;}
+               public string Firmware            {get;set;}
+               public string Operating_State     {get;set;}
+               public string Station_Name        {get;set;}
+               public string SMC_SerialNo        {get;set;}
+               public string Plant_DesignationId {get;set;}
+               public string Location_Id         {get;set;}
+               public bool? IsPingable          {get;set;}
+               public bool? IsFaulty            {get;set;}
+    }
+}
