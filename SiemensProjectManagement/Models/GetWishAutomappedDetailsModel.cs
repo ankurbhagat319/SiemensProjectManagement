a202df5dea358898ead535f7fb64c42aea2f1cc2
K, Ajith Shenoy (ADV D AA DDI HMI-P S7PLC) <ajith.k@siemens.com>
2021-07-29 11:30:05 +0530

    Added Wishlist pages.

---
+++ SiemensProjectManagement/Models/GetWishAutomappedDetailsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiemensProjectManagement.Models
+{
+    public class GetWishAutomappedDetailsModel
+    {
+        public int WishlistId { get; set; }
+        public string WishTypeName { get; set; }
+        public string ProjectName { get; set; }
+        public string AssetType_Name { get; set; }
+        public string Asset { get; set; }
+        public int AvailableQuantity { get; set; }
+        public string Description { get; set; }
+        public int? RequestDuration { get; set; }
+        public int? AvailableDuration { get; set; }
+    }
+}
---
+++ SiemensProjectManagement/Models/WishListDetailsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiemensProjectManagement.Models
+{
+    public class WishListDetailsModel
+    {
+        public int WishlistId { get; set; }
+        public string WishTypeName { get; set; }
+        public string UserName { get; set; }
+        public string ProjectName { get; set; }
+        public string AssetType_Name { get; set; }
+        public string Asset { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public string RequestDuration { get; set; }
+    }
+}
